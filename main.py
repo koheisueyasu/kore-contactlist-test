2f45ff2cd5acdb71c4866471fda2484a65cef4c9
koheisueyasu <kohei.sueyasu@assurant.com>
2026-03-30 12:28:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ app = FastAPI()
 
 @app.post("/contacts")
 def get_contacts():
-    return {
+    return [
+                {
                     "OFFER_RMA_NUMBER": "RKSU95050000519999",
                     "OFFER_DATE": "20260329",
                     "CUSTOMER_IDENTIFICATION": "2023022009999",
-                    "CUSTOMER_LASTNAME": "Sueyasu",
-                    "CUSTOMER_FIRSTNAME": "API Test",
+                    "CUSTOMER_LASTNAME": "末安",
+                    "CUSTOMER_FIRSTNAME": "APIテスト",
                     "OFFER_MODEL": "iPhone SE 3 256GB Midnight - Rakuten",
                     "OFFER_SERIAL_NUMBER": "350158429809999",
                     "MOBILE_NUMBER": "+819018919939",
@@ -18,5 +19,19 @@ def get_contacts():
                     "RECEIPT_DEVICE_STATUS": "NR01",
                     "TRACKING_NUMBER": "",
                     "RETURN_DEADLINE": "2026/3/28",
-                }
-            
+                },
+                {
+                    "OFFER_RMA_NUMBER": "RKSU95050000518888",
+                    "OFFER_DATE": "20260308",
+                    "CUSTOMER_IDENTIFICATION": "2023022008888",
+                    "CUSTOMER_LASTNAME": "APIテスト",
+                    "CUSTOMER_FIRSTNAME": "興平",
+                    "OFFER_MODEL": "iPhone 13 Pro 256GB Midnight",
+                    "OFFER_SERIAL_NUMBER": "350158429808888",
+                    "MOBILE_NUMBER": "+819011111111",
+                    "ORIGINAL_START_DATE": "2026/3/8",
+                    "RECEIPT_DEVICE_STATUS": "NR03",
+                    "TRACKING_NUMBER": "",
+                    "RETURN_DEADLINE": "2026/3/28",
+                },
+            ]
